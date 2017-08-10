785bf74b4d0b907477376761bca9e7a06702ec67
chriddyp <chris@plot.ly>
2017-08-10 18:30:25 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dash On-Premise Sample App
+
+This repository contains a simple Dash app that can be deployed as-is on your [Plotly On-Premise](https://plot.ly/products/on-premise) server.
+
+To learn more about deploying applications on Plotly On-Premises, view the [Plotly On-Premise Dash App Deployment Docs](https://plot.ly/dash/deployment/on-premise)
---
+++ app.py
@@ -0,0 +1,36 @@
+import dash
+import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+import plotly.plotly as py
+
+import config
+
+app = dash.Dash(__name__)
+
+# Expose the server variable
+server = app.server
+
+# Serve JS and CSS files locally instead of from global CDN
+app.scripts.config.serve_locally = True
+app.css.config.serve_locally = True
+
+# Standard Dash app code below
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
