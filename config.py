06b6881fb67237202804f68ba87797842822b615
Chris Parmer <chris@plot.ly>
2017-08-11 16:52:19 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+# Replace with the name of your Dash app
+# This will end up being part of the URL of your deployed app,
+# so it can't contain any spaces, capitalizations, or special characters
+#
+# This name MUST match the name that you specified in the
+# Dash App Manager
+DASH_APP_NAME = 'my-dash-app'
+
+# Dash On-Premise is configured with either "Path based routing" 
+# or "Domain based routing"
+# Ask your server administrator which version was set up.
+# If a separate subdomain was created,
+# then set this to `False`. If it was not, set this to 'True'.
+# Path based routing is the default option and most On-Premise
+# users use this option.
+PATH_BASED_ROUTING = 'True'
+
+DASH_APP_PRIVACY = 'private'
+
+# Fill in with your Plotly On-Premise username
+os.environ['PLOTLY_USERNAME'] = 'your-plotly-username'
+
+# Fill in with your Plotly On-Premise API key
+# See <your-plotly-server>/settings/api to generate a key
+# If you have already created a key and saved it on your own machine
+# (from the Plotly-Python library instructions at https://plot.ly/python/getting-started)
+# then you can view that key in your ~/.plotly/.config file or by running:
+# `python -c import plotly; print(plotly.tools.get_config_file())`
+os.environ['PLOTLY_API_KEY'] = 'your-plotly-api-key'
+
+# Fill in with your Plotly On-Premise domain
+os.environ['PLOTLY_DOMAIN'] = 'https://your-plotly-domain.com'
+os.environ['PLOTLY_API_DOMAIN'] = os.environ['PLOTLY_DOMAIN']
+
+# Fill in with the domain of your Dash subdomain.
+# This matches the domain of the Dash App Manager
+PLOTLY_DASH_DOMAIN='https://your-dash-manager-plotly-domain.com'
+
+# Keep as True if your SSL certificates are valid.
+# If you are just trialing Plotly On-Premise with self signed certificates,
+# then you can set this to False. Note that self-signed certificates are not
+# safe for production.
+os.environ['PLOTLY_SSL_VERIFICATION'] = 'True'
