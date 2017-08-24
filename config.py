673d465225d30b057fc76f0464f27cba65a8aab9
chriddyp <chris@plot.ly>
2017-08-24 13:33:11 -0400

    config

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 #
 # This name MUST match the name that you specified in the
 # Dash App Manager
-DASH_APP_NAME = 'name-of-your-dash-app'
+DASH_APP_NAME = 'chris-content-length'
 
 # Dash On-Premise is configured with either "Path based routing" 
 # or "Domain based routing"
@@ -15,7 +15,7 @@ DASH_APP_NAME = 'name-of-your-dash-app'
 # then set this to `False`. If it was not, set this to 'True'.
 # Path based routing is the default option and most On-Premise
 # users use this option.
-PATH_BASED_ROUTING = True
+PATH_BASED_ROUTING = False
 
 # Set to `private` if you want to add a login screen to your app
 # You can provision who can view the app in your list of files at <your-plotly-server>/organize
