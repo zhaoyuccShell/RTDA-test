aeb934792123bef78c068c06a97855fee8e0d856
Zhao <Yucheng.Zhao@shell.com>
2018-06-29 14:08:57 -0500

    test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import config
 from auth import auth
 from utils import StaticUrlPath
 
-root = './lib/PWitsml'
+root = '/rtda-test/lib/PWitsml'
 classpath = root + '/activation.jar;' + \
             root + '/javax.mail.jar;' + \
             root + '/jwitsml-1.0.jar;' + \
